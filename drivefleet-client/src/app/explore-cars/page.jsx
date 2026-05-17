393e1799c58d4c64096e3f5a7e666467ed6cc76f
Sakibur59 <sakiburrahman5978@gmail.com>
2026-05-17 23:57:23 +0600

    explore car section added

--- drivefleet-client/src/app/explore-cars/page.jsx
+++ drivefleet-client/src/app/explore-cars/page.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import CarCard from "@/components/CarCard";
 
-const AllCarsPage = () => {
-    return (
-        <div>
-            sdhbf
-        </div>
-    );
+
+const AllCarPage = async () => {
+  const res = await fetch(`http://localhost:5000/cars`);
+  const cars = await res.json();
+  return (
+    <div className="mt-20">
+      <div className="max-w-7xl mx-auto">
+        <h1 className="text-3xl font-bold mb-6">Explore All Cars</h1>
+        <p>Find your next adventure among our curated selection of cars.</p>
+      </div>
+
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-8 mt-10">
+        {cars.map((car) => (
+          <CarCard key={car._id} car={car} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default AllCarsPage;
+export default AllCarPage;
---
+++ drivefleet-client/src/components/CarCard.jsx
@@ -0,0 +1,63 @@
+import { Button } from "@heroui/react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { LuMap, LuMapPin, LuUsersRound } from "react-icons/lu";
+
+
+const CarCard = ({ car }) => {
+  const {
+    _id,
+    carName,
+    category,
+    imageUrl,
+    pricePerDay,
+    location,
+    description,
+    availability,
+    seatingCapacity,
+  } = car;
+  return (
+    <div className="border mb-8 rounded-md  shadow-lg">
+      <Image
+        src={imageUrl}
+        alt={carName}
+        width={400}
+        height={300}
+        className="w-full h-48 object-cover rounded-t-md"
+      />
+      <div className="p-4">
+        <div className="flex items-center gap-2 text-gray-500 mb-2">
+          <h3 className="font-bold text-cyan-600">{category}</h3>
+        </div>
+        <div className="flex justify-between ">
+          <div>
+            <div>
+              <h2 className="text-xl font-bold">{carName}</h2>
+            </div>
+            <p className="text-gray-500 mt-2">{description}</p>
+            <div className="flex items-center  mt-2">
+              <LuMapPin className="text-gray-500" />
+              <span className="text-gray-500 ml-1">{location}</span>
+            </div>
+            <div className="flex items-center gap-2 mt-2">
+              <LuUsersRound />
+              <p className="text-gray-500">{seatingCapacity} Seats</p>
+            </div>
+          </div>
+          <div>
+            <h3 className="text-lg font-semibold">${pricePerDay}/Day</h3>
+          </div>
+        </div>
+
+        <Link href={`/explore-cars/${_id}`} >
+          <Button variant="primary" className="mt-3 text-white w-full">
+            View Details
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default CarCard;
