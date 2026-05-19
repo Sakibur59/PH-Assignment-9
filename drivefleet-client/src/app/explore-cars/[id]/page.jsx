e198706e5b4564bc5d2f662dc03c7f0020e1da90
Sakibur59 <sakiburrahman5978@gmail.com>
2026-05-19 14:04:50 +0600

    booking count added

--- drivefleet-client/src/app/explore-cars/[id]/page.jsx
+++ drivefleet-client/src/app/explore-cars/[id]/page.jsx
@@ -5,8 +5,14 @@ import { LuMapPin, LuUsersRound } from "react-icons/lu";
 
 const CarDetailsPage = async ({ params }) => {
   const { id } = await params;
-  const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/cars/${id}`);
-  const car = await res.json();
+  const [carRes, countRes] = await Promise.all([
+    fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/cars/${id}`),
+    fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/bookings-count/${id}`, {
+      cache: "no-store",
+    }),
+  ]);
+  const car = await carRes.json();
+  const { count } = await countRes.json();
   const {
     _id,
     carName,
@@ -33,27 +39,36 @@ const CarDetailsPage = async ({ params }) => {
           ></Image>
         </div>
         <div className="shadow p-10 rounded-md h-full">
-            <h1 className="text-3xl font-bold mt-5">{carName}</h1>
-            <p className="text-gray-500 mt-2">{description}</p>
-            <div className="flex items-center gap-10 mt-4">
-                <h3 className="text-lg font-semibold text-cyan-600">
-                    Category: {category}</h3>
-                    <p className="font-bold">FuelType: {fuelType}</p>
-            </div>
-                <div className="flex items-center gap-2 mt-4">
-                    <LuMapPin className="text-gray-500" />
-                    <span className="text-gray-500">{location}</span>
-                </div>
-                <div className="flex items-center gap-2">
-                    <LuUsersRound className="text-gray-500" />
-                    <span className="text-gray-500">{seatingCapacity} Seats</span>
-                </div>
-            
-            <h2 className="text-xl font-semibold mt-4">${pricePerDay}/Day</h2>
-            <p className="text-gray-500 mt-2">Availability: {availability ? "Available" : "Not Available"}</p>
-            
-        <BookingModal car={car} />
-      
+          <h1 className="text-3xl font-bold mt-5">{carName}</h1>
+          <p className="text-gray-500 mt-2">{description}</p>
+          <div className="flex items-center gap-10 mt-4">
+            <h3 className="text-lg font-semibold text-cyan-600">
+              Category: {category}
+            </h3>
+            <p className="font-bold">FuelType: {fuelType}</p>
+          </div>
+          <div className="flex items-center gap-2 mt-4">
+            <LuMapPin className="text-gray-500" />
+            <span className="text-gray-500">{location}</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <LuUsersRound className="text-gray-500" />
+            <span className="text-gray-500">{seatingCapacity} Seats</span>
+          </div>
+
+          <h2 className="text-xl font-semibold mt-4">${pricePerDay}/Day</h2>
+          <p className="text-gray-500 mt-2">
+            Availability: {availability ? "Available" : "Not Available"}
+          </p>
+
+          <div className="flex items-center gap-2 ">
+            <span>🧳</span>
+            <span>
+              <strong className="text-black">Total Booked: {count}</strong>
+            </span>
+          </div>
+
+          <BookingModal car={car} />
         </div>
       </div>
     </div>
