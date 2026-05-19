f45b218b38abb669c814ae66ef754016a1603b21
Sakibur59 <sakiburrahman5978@gmail.com>
2026-05-19 13:47:49 +0600

    add some changes in my added car page

--- drivefleet-client/src/app/my-added-cars/page.jsx
+++ drivefleet-client/src/app/my-added-cars/page.jsx
@@ -2,6 +2,7 @@ import { AddCarDeleteAlert } from "@/components/AddCarDeleteModal";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import Image from "next/image";
+import Link from "next/link";
 
 
 const MyAddedCarPage = async () => {
@@ -19,6 +20,7 @@ const MyAddedCarPage = async () => {
         Private Layout
       </p>
       <h1 className="text-4xl font-black mb-8">My Added Cars</h1>
+      
 
       {!cars?.length ? (
         <p>No cars added yet.</p>
@@ -38,6 +40,9 @@ const MyAddedCarPage = async () => {
                 <p className="text-xl font-bold text-cyan-500">
                   Price: ${car.pricePerDay}/Day
                 </p>
+                <p>Brand: {car.brand}</p>
+                <p>Category: {car.category}</p>
+                <p>Availability: {car.availability.toUpperCase()}</p>
 
                 <AddCarDeleteAlert carId={car._id} />
               </div>
@@ -45,6 +50,11 @@ const MyAddedCarPage = async () => {
           ))}
         </div>
       )}
+      <div className="flex justify-end mt-10">
+        <Link href="/add-car" className="inline-block mb-6 px-6 py-3 bg-cyan-500 text-white rounded-md shadow hover:bg-cyan-600 transition">
+          + Add New Car
+        </Link>
+      </div>
     </div>
   );
 };
